e3cb97c244507979d39a88460940a80d38370ca6
Roger Peratello <roger.peratello@hotmail.com>
2021-05-23 00:54:15 -0300

    gerado componentes e service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarComponent } from './listar/listar.component';
+import { DeletarComponent } from './deletar/deletar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { AlterarComponent } from './alterar/alterar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarComponent,
+    DeletarComponent,
+    CadastrarComponent,
+    AlterarComponent
   ],
   imports: [
     BrowserModule,
